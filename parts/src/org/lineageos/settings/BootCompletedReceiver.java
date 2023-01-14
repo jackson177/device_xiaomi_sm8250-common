85af01948d05811deab4f1ef6e697c1bef3a6b43
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2023-01-13 20:30:12 -0500

    sm8250-common: Implement HBM & Auto HBM
    * parts: Implement HBM & Auto HBM
    * sepolicy: Set HBM permissions
    * parts: Disable hbm thermal throttle
    * sepolicy: Allow devicesettings write to vendor_sysfs_graphics
    * parts: hbm: Fix compilation for QPR1 merge
    
    Co-Authored-By: Max Weninger <max.weninger@gmail.com>
    Co-Authored-By: AnierinB <anierin@evolution-x.org>
    Co-Authored-By: EmanuelCN <emanuelghub@gmail.com>
    Co-Authored-By: johnmart19 <johnivan19999@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -26,6 +26,7 @@ import android.util.Log;
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
+import org.lineageos.settings.utils.FileUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -43,5 +44,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         }
         ThermalUtils.startService(context);
         RefreshUtils.startService(context);
+        FileUtils.enableService(context);
     }
 }
